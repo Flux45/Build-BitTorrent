72d0d4ebb33769c8acdd66117e67c2e750024bef
Flux <aayyuusshh.45@gmail.com>
2024-10-10 15:26:14 +0530

    Discover Peers

--- app/main.py
+++ app/main.py
@@ -183,28 +183,47 @@ def main():
         )
         print(httpget(url, query_params))
 
-    elif command == "peers":
-        file_name = sys.argv[2]
-        with open(file_name, "rb") as torrent_file:
-            bencoded_content = torrent_file.read()
-        torrent = decode_bencode(bencoded_content)
-        url = torrent["announce"].decode()
-        query_params = dict(
-            info_hash=hashlib.sha1(bencode(torrent["info"])).digest(),
-            peer_id="00112233445566778899",
-            port=6881,
-            uploaded=0,
-            downloaded=0,
-            left=torrent["info"]["length"],
-            compact=1,
-        )
-        response = decode_bencode(requests.get(url, params=query_params).content)
-        peers = response["peers"]
-        for i in range(0, len(peers), 6):
-            peer = peers[i: i + 6]
-            ip_address = f"{peer[0]}.{peer[1]}.{peer[2]}.{peer[3]}"
-            port = int.from_bytes(peer[4:], byteorder="big", signed=False)
-            print(f"{ip_address}:{port}")
+elif command == "peers":
+
+file_name = sys.argv[2]
+
+with open(file_name, "rb") as torrent_file:
+    bencoded_content = torrent_file.read()
+
+torrent = decode_bencode(bencoded_content)
+
+url = torrent["announce"].decode()
+
+query_params = dict(
+
+    info_hash=hashlib.sha1(bencode(torrent["info"])).digest(),
+
+    peer_id="00112233445566778899",
+
+    port=6881,
+
+    uploaded=0,
+
+    downloaded=0,
+
+    left=torrent["info"]["length"],
+
+    compact=1,
+
+)
+
+response = decode_bencode(requests.get(url, query_params).content)
+
+peers = response["peers"]
+
+for i in range(0, len(peers), 6):
+    peer = peers[i: i + 6]
+
+    ip_address = f"{peer[0]}.{peer[1]}.{peer[2]}.{peer[3]}"
+
+    port = int.from_bytes(peer[4:], byteorder="big", signed=False)
+
+    print(f"{ip_address}:{port}")
     else:
         raise NotImplementedError(f"Unknown command {command}")
 
